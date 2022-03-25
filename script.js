1092750668010441ba6c6c4bde8ffa43e7bf080a
Wian <w.ian.victor@icloud.com>
2022-03-24 22:33:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  }
+  function myFunction2() {
+    /* Get the text field */
+    var copyText2 = document.getElementById("myInput2");
+  
+    /* Select the text field */
+    copyText2.select();
+    copyText2.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText2.value);
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText2.value);
+  }
+  function myFunction3() {
+    /* Get the text field */
+    var copyText3 = document.getElementById("myInput3");
+  
+    /* Select the text field */
+    copyText3.select();
+    copyText3.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText3.value);
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText3.value);
+  }
+  function myFunction4() {
+    /* Get the text field */
+    var copyText4 = document.getElementById("myInput4");
+  
+    /* Select the text field */
+    copyText4.select();
+    copyText4.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText4.value);
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText4.value);
+  }
+  function myFunction5() {
+    /* Get the text field */
+    var copyText5 = document.getElementById("myInput5");
+  
+    /* Select the text field */
+    copyText5.select();
+    copyText5.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText5.value);
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText5.value);
+  }
+  function myFunction6() {
+    /* Get the text field */
+    var copyText6 = document.getElementById("myInput6");
+  
+    /* Select the text field */
+    copyText6.select();
+    copyText6.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText6.value);
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText6.value);
+  }
